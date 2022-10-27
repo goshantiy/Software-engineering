946255876642a805246e7ddb461ad1019dc92575
Georgiy <goshantiy@gmail.com>
2022-10-27 23:27:52 +0700

    password encryption

--- auth.php
+++ auth.php
@@ -1,96 +1,79 @@
 <?php
-$data = $_POST;
 include 'bdconnect.php';
+if ( !session_id() )
 session_start();
-	if(isset($data['btn_login_form']))
-     {
-        if(trim($data['form-login-name']) == '')
-         {
-			$errors[] = "Введите логин!";
-		}
-        if(trim($data['form-password']) == '') 
-        {
-			$errors[] = "Введите пароль!";
-		}
-        if(trim($data['captcha'])!= '')
-        {
-                $captcha = trim($data['captcha']);
-                if(($_SESSION["rand"] != $captcha) && ($_SESSION["rand"] != ""))
-                {
-                $errors[]= "Вы ввели неправильную капчу ";
-                }            
-        }
-            else
-            {
-            $errors[]="Поле для ввода капчи не должно быть пустым";
-            }
-        if(empty($errors))
-        {
-        	// экранирования символов для mysql
-        $link = mysqli_connect($host, $user, $password, $database) or die("Ошибка " . mysqli_error($link)); 
-        $login = htmlspecialchars(mysqli_real_escape_string($link, $_POST['form-login-name']));
-		$password = htmlspecialchars(mysqli_real_escape_string($link, $_POST['form-password']));
-        $query ="SELECT * FROM user WHERE `user_login`='$login' AND `password` = '$password'";
-        $result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link));	
-        if(mysqli_num_rows($result) > 0 && $result)
-            {
-                $res = mysqli_fetch_row($result);
-                $_SESSION['user_id']=$res[0];   
-                $_SESSION['role']=$res[5];
-                $_SESSION['login'] = $login;
-                header('Location:/index.php');
-            }
-        else
-        {
-        $errors[]= "Неправильный логин или пароль";
-        }
-        }
-            echo '<div style="color: red; display:flex; justify-content: center; ">'.array_shift($errors).'</div><hr>';
+if (isset($_POST['btn_login_form'])) {
+    if (trim($_POST['form-login-name']) == '') {
+        $errors[] = "Введите логин!";
     }
+    if (trim($_POST['form-password']) == '') {
+        $errors[] = "Введите пароль!";
+    }
+    if (trim($_POST['captcha']) != '') {
+        $captcha = trim($_POST['captcha']);
+        if (($_SESSION["rand"] != $captcha) && ($_SESSION["rand"] != "")) {
+            $errors[] = "Вы ввели неправильную капчу";
+        }
+    } else {
+        $errors[] = "Поле для ввода капчи не должно быть пустым";
+    }
+    if (empty($errors)) {
+        // экранирования символов для mysql
+        $link = mysqli_connect($host, $user, $password, $database) or die("Ошибка " . mysqli_error($link));
+        $login = strip_tags(htmlspecialchars(mysqli_real_escape_string($link, $_POST['form-login-name'])));
+        $password = $_POST['form-password'];
+        $query = "SELECT * FROM user WHERE `user_login`='$login'";
+        $result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link));
+        $res = mysqli_fetch_assoc($result);
+        if (password_verify($password,$res['password'])) {
+            $_SESSION['user_id'] = $res['user_id'];
+            $_SESSION['role'] = $res['role'];
+            $_SESSION['login'] = $login;
+            header('Location:/index.php');
+            exit();
+        } else {
+            $errors[] = "Неправильный логин или пароль";
+        }
+    }
+    if(!empty($errors))
+    echo '<div style="color: red; display:flex; justify-content: center; ">' . array_shift($errors) . '</div><hr>';
+}
 ?>
-
-
-
-
-
-
 <!DOCTYPE html>
 <html lang="ru">
-<head>
-	<!-- Stylesheets
-	============================================= -->
-	<link href="https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:wght@100&display=swap" rel="stylesheet">
-	<link rel="stylesheet" href="/css/reg.css" type="text/css">
-	<!-- Document Title
-	============================================= -->
-	<title>Вход</title>
-   
-</head>
-<body>
+    <head>
+        <link href="https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:wght@100&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="/css/reg.css" type="text/css">
+        <title>Вход</title>
+
+    </head>
+    <body>
         <div class="container positioner">
-                        <h1 class="center">Authorization</h1>
-                                    <h3 class="center">Enter your information</h3>
-        <form class = center action="auth.php" class="row"  method="POST">
-        <div class = left>
-            <div class="col-6 form-group">
-            <p class=form-login>Login:</p>
-            <input type="text" id="form-login-name" name="form-login-name" value="" class="form-control" placeholder="User" required/>
-            </div>
-            <div class="col-6 form-group">
-            <p class=form-password>Password:</p>
-            <input type="password" id="form-password" name="form-password" value="" class="form-control" required/>
-            </div>
-            <div class="col-6 form-group">
-            <p class=form-password>Captcha:</p>
-            <div class=center> <img src="captcha.php"/></div>  
-            <input class="center" type="text" id="captcha" name="captcha" placeholder="Captcha" class="form-control" required/>
-            </div>   
-        <div class=" center form-group">
-        <button class="btn btn-dark m-0" id="btn_login_form" name="btn_login_form" value="Login">Вход</button>
+            <div class="positioner-inside">
+            <h1 class="center">Authorization</h1>
+            <h3 class="center">Enter your information</h3>
+            <form class = center action="auth.php" class="row"  method="POST">
+                <div class = left>
+                    <div class="col-6 form-group">
+                        <p class=form-login>Login:</p>
+                        <input type="text" id="form-login-name" name="form-login-name" value="" class="form-control" placeholder="User" required/>
+                    </div>
+                    <div class="col-6 form-group">
+                        <p class=form-password>Password:</p>
+                        <input type="password" id="form-password" name="form-password" value="" class="form-control" required/>
+                    </div>
+                    <div class="col-6 form-group">
+                        <p class=form-password>Captcha:</p>
+                        <div class=center> <img src="captcha.php"/></div>  
+                        <input class="center" type="text" id="captcha" name="captcha" placeholder="Captcha" class="form-control" required/>
+                    </div>   
+                    <div class=" center form-group">
+                        <button class="btn btn-dark m-0" id="btn_login_form" name="btn_login_form" value="Login">Вход</button>
+                    </div>
+                </div>                
+            </form>  
+            <p class="center">Not registered yet?<a href=/login-register.php>Registration</a></p> 
         </div>
-        </div>                
-        </form>  
-        <p class="center">Not registered yet?<a href=/login-register.php>Registration</a></p> 
         </div>
-</body>
+    </body>
 </html>
--- login-register.php
+++ login-register.php
@@ -1,141 +1,141 @@
 <?php
-	include 'bdconnect.php';
-	$form_status_register = "Не зарегистрованный пользователь!";
-	session_start();	
-	// Переменная для сбора данных с формы
-	$data = $_POST;
-	// Выполнения кода после активации кнопки "Зарегистрироваться" с формы
-	if(isset($data['btn_registr_form'])) {
-		//Проверка данных формы
-		if(trim($data['form-name']) == '') {
-			$errors[] = "Введите ФИО пользователя!";
-		}
-		
-		if(trim($data['form-login-name']) == '') {
-			$errors[] = "Введите логин пользователя!";
-		}  
-		if(trim($data['form-email']) == '') {
-			$errors[] = "Введите E-mail пользователя!";
-		}  
-		if($data['formemailpass'] != $data['form-email']) {
-			$errors[] = "Повторный email не совпадает!";
-		}  
-		if($data['form-password'] == '') {
-			$errors[] = "Введите пароль пользователя!";
-		}  
-		if($data['form-password-pass'] != $data['form-password']) {
-			$errors[] = "Повторный пароль не совпадает!";
-		}  
+include 'bdconnect.php';
+if ( !session_id() )
+session_start();
+$form_status_register = "Не зарегистрованный пользователь!";
+// Переменная для сбора данных с формы
+$_POST = $_POST;
+// Выполнения кода после активации кнопки "Зарегистрироваться" с формы
+if (isset($_POST['btn_registr_form'])) {
+    //Проверка данных формы
+    if (trim($_POST['form-name']) == '') {
+        $errors[] = "Введите ФИО пользователя!";
+    }
 
-		// функция mb_strlen - получает длину строки
-        // Если логин будет меньше 5 символов и больше 90, то выйдет ошибка
-		if(mb_strlen($data['form-login-name']) < 5 || mb_strlen($data['form-login-name']) > 90) {
-	    	$errors[] = "Недопустимая длина логина";
-    	}
-    	if (mb_strlen($data['form-password']) < 6 || mb_strlen($data['form-password']) > 64){
-	    	$errors[] = "Недопустимая длина пароля (от 6 до 64 символов)";
-    	}
-    	// проверка на правильность написания Email
-    	if (!preg_match("/[0-9a-z_]+@[0-9a-z_^\.]+\.[a-z]{2,3}/i", $data['form-email'])) {
-	    	$errors[] = 'Неверно введен е-mail';
-    	}
+    if (trim($_POST['form-login-name']) == '') {
+        $errors[] = "Введите логин пользователя!";
+    }
+    if (trim($_POST['form-email']) == '') {
+        $errors[] = "Введите E-mail пользователя!";
+    }
+    if ($_POST['formemailpass'] != $_POST['form-email']) {
+        $errors[] = "Повторный email не совпадает!";
+    }
+    if ($_POST['form-password'] == '') {
+        $errors[] = "Введите пароль пользователя!";
+    }
+    if ($_POST['form-password-pass'] != $_POST['form-password']) {
+        $errors[] = "Повторный пароль не совпадает!";
+    }
 
-		$link = mysqli_connect($host, $user, $password, $database) or die("Ошибка " . mysqli_error($link)); 
-		// экранирования символов для mysql
-		$name = strip_tags(htmlspecialchars(mysqli_real_escape_string($link, $_POST['form-name'])));
-		$login = strip_tags(htmlspecialchars(mysqli_real_escape_string($link, $_POST['form-login-name'])));
-		$password = strip_tags(htmlspecialchars(mysqli_real_escape_string($link, $_POST['form-password'])));
-		$email = strip_tags(htmlspecialchars(mysqli_real_escape_string($link, $_POST['form-email'])));
-		//$session_key = $sessionid;
-		$query ="SELECT `user_login` FROM user WHERE `user_login`='$login'";
-		$result = mysqli_query($link, $query);	
-		if(mysqli_num_rows($result) > 0)
-		$errors[]="Этот логин занят!";
+    // функция mb_strlen - получает длину строки
+    // Если логин будет меньше 5 символов и больше 90, то выйдет ошибка
+    if (mb_strlen($_POST['form-login-name']) < 5 || mb_strlen($_POST['form-login-name']) > 90) {
+        $errors[] = "Недопустимая длина логина";
+    }
+    if (mb_strlen($_POST['form-password']) < 6 || mb_strlen($_POST['form-password']) > 64) {
+        $errors[] = "Недопустимая длина пароля (от 6 до 64 символов)";
+    }
+    // проверка на правильность написания Email
+    if (!preg_match("/[0-9a-z_]+@[0-9a-z_^\.]+\.[a-z]{2,3}/i", $_POST['form-email'])) {
+        $errors[] = 'Неверно введен е-mail';
+    }
 
-		$query ="SELECT `user_email` FROM user WHERE `user_email`='$email'";
-		$result = mysqli_query($link, $query);	
-		if(mysqli_num_rows($result) > 0)
-		$errors[]="Этот email занят!";
-		if(empty($errors)) {	
-			// создание строки запроса
-			$query ="INSERT INTO user(`user_id`, `user_login`, `user_email`, `user_name`, `password`, `role`) VALUES(NULL, '$login','$email','$name', '$password', 1)";
+    $link = mysqli_connect($host, $user, $password, $database) or die("Ошибка " . mysqli_error($link));
+    // экранирования символов для mysql
+    $name = strip_tags(htmlspecialchars(mysqli_real_escape_string($link, $_POST['form-name'])));
+    $login = strip_tags(htmlspecialchars(mysqli_real_escape_string($link, $_POST['form-login-name'])));
+    $password = password_hash($_POST['form-password'],PASSWORD_DEFAULT);
+    $email = strip_tags(htmlspecialchars(mysqli_real_escape_string($link, $_POST['form-email'])));
+    //$session_key = $sessionid;
+    $query1 = "SELECT `user_login` FROM user WHERE `user_login`='$login'";
+    $result1 = mysqli_query($link, $query1);
+    if (mysqli_num_rows($result1) > 0) {
+        $errors[] = "Этот логин занят!";
+    }
 
-		// выполняем запрос
-			$result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link)); 
-			if($result)
-			{
-				echo "<span style='color:green; display:flex; justify-content: center;'>Данные пользователя добавлены</span>";
-			}
+    $query2 = "SELECT `user_email` FROM user WHERE `user_email`='$email'";
+    $result2 = mysqli_query($link, $query2);
+    if (mysqli_num_rows($result2) > 0) {
+        $errors[] = "Этот email занят!";
+    }
+    if (empty($errors)) {
+        // создание строки запроса
+        $query = "INSERT INTO user(`user_id`, `user_login`, `user_email`, `user_name`, `password`, `role`) VALUES(NULL, '$login','$email','$name', '$password', 1)";
 
-			
-		// закрываем подключение
-			mysqli_close($link);
+        // выполняем запрос
+        $result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link));
+        if ($result) {
+            echo "<span style='color:green; display:flex; justify-content: center;'>Данные пользователя добавлены</span>";
+        }
 
-		} else {
-					// array_shift() извлекает первое значение массива array и возвращает его, сокращая размер array на один элемент. 
-			echo '<div style="color: red; display:flex; justify-content: center; ">' . array_shift($errors). '</div><hr>';
-		}
 
-	}
+        // закрываем подключение
+        mysqli_close($link);
+    } else {
+        // array_shift() извлекает первое значение массива array и возвращает его, сокращая размер array на один элемент. 
+        echo '<div style="color: red; display:flex; justify-content: center; ">' . array_shift($errors) . '</div><hr>';
+    }
+}
 ?>
 <head>
-	<!-- Stylesheets
-	============================================= -->
-	<link href="https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:wght@100&display=swap" rel="stylesheet">
-	<link rel="stylesheet" href="/css/reg.css" type="text/css">
-	<!-- Document Title
-	============================================= -->
-	<title>Регистрация пользователя</title>
+    <!-- Stylesheets
+    ============================================= -->
+    <link href="https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:wght@100&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/css/reg.css" type="text/css">
+    <!-- Document Title
+    ============================================= -->
+    <title>Регистрация пользователя</title>
 
 </head>
 
 <body>
-			<div class="container positioner">
-				<h1 class="center">Registration</h1>
-							<h3 class="center">Enter your information </h3>
-							<div class="center">
-								
-								</div>
+    <div class="container positioner">
+        <h1 class="center">Registration</h1>
+        <h3 class="center">Enter your information </h3>
+        <div class="center">
 
-							<form class =center action="login-register.php" class="row"  method="POST">
-<div class="left">
-								<div class="col-6 form-group">
-									<p>Full name:</p>
-									<input type="text" id="form-name" name="form-name" value="" class="form-control" placeholder="Иванов Иван Иванович" required/>
-								</div>
+        </div>
 
-								<div class="col-6 form-group">
-									<p>Mail:</p>
-									<input type="text" id="form-email" name="form-email" value="" class="form-control" placeholder="Example@mail.ru" required/>
-								</div>
+        <form class =center action="login-register.php" class="row"  method="POST">
+            <div class="left">
+                <div class="col-6 form-group">
+                    <p>Full name:</p>
+                    <input type="text" id="form-name" name="form-name" value="" class="form-control" placeholder="Иванов Иван Иванович" required/>
+                </div>
 
-								<div class="col-6 form-group">
-									<p>Login:</p>
-									<input type="text" id="form-login-name" name="form-login-name" value="" class="form-control" placeholder="User" required/>
-								</div>
-</div>
-<div class="right">
-								<div class="col-6 form-group">
-									<p>Confirm mail:</p>
-									<input type="text" id="formemailpass" name="formemailpass" value="" class="form-control" placeholder="Example@mail.ru" required/>
-								</div>
+                <div class="col-6 form-group">
+                    <p>Mail:</p>
+                    <input type="text" id="form-email" name="form-email" value="" class="form-control" placeholder="Example@mail.ru" required/>
+                </div>
+
+                <div class="col-6 form-group">
+                    <p>Login:</p>
+                    <input type="text" id="form-login-name" name="form-login-name" value="" class="form-control" placeholder="User" required/>
+                </div>
+            </div>
+            <div class="right">
+                <div class="col-6 form-group">
+                    <p>Confirm mail:</p>
+                    <input type="text" id="formemailpass" name="formemailpass" value="" class="form-control" placeholder="Example@mail.ru" required/>
+                </div>
 
 
-								<div class="col-6 form-group">
-									<p>Password:</p>
-									<input type="password" id="form-password" name="form-password" value="" class="form-control" required/>
-								</div>
+                <div class="col-6 form-group">
+                    <p>Password:</p>
+                    <input type="password" id="form-password" name="form-password" value="" class="form-control" required/>
+                </div>
 
-								<div class="col-6 form-group">
-									<p>Confirm password:</p>
-									<input type="password" id="form-password-pass" name="form-password-pass" value="" class="form-control" required />
-								</div>
-</div>
-								<div class="col-12 center">
-									<button class="btn btn-dark m-0" id="btn_registr_form" name="btn_registr_form" value="register">Submit</button>
-								</div>
-								</form>		
-		<p class=center>Already have account? <a href=/auth.php>Authorization</a></p>
-</div>
+                <div class="col-6 form-group">
+                    <p>Confirm password:</p>
+                    <input type="password" id="form-password-pass" name="form-password-pass" value="" class="form-control" required />
+                </div>
+            </div>
+            <div class="col-12 center">
+                <button class="btn btn-dark m-0" id="btn_registr_form" name="btn_registr_form" value="register">Submit</button>
+            </div>
+        </form>		
+        <p class=center>Already have account? <a href=/auth.php>Authorization</a></p>
+    </div>
 </body>
 </html>
